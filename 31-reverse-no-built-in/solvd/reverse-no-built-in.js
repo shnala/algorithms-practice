143c2f47bd9f28857760c5d3b6cba1d161f918c6
Shawn Analla <shawnanalla@gmail.com>
2022-04-13 15:47:55 -0700

    week8 wip

---
+++ 31-reverse-no-built-in/solvd/reverse-no-built-in.js
@@ -0,0 +1,18 @@
+// Write code to return a new string with all of the characters in `str` reversed.
+// You may NOT use the built-in reverse method
+
+const str = "just keep swimming";
+
+const reverse = (str) => {
+    let reversed = [];
+    for (let i = str.length-1; i >= 0; i--) {
+        const character = str[i];
+        reversed.push(character);
+    };
+    const result = reversed.join("");
+    return result;
+};
+
+console.log(reverse(str));
+
+//done
