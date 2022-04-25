1253140a80032120ffe336c7885183293faa2b6d
Shawn Analla <shawnanalla@gmail.com>
2022-04-25 12:56:33 -0700

    caesar cipher needs revisiting

--- 45-caesar-cipher/Solved/caesar-cipher.js
+++ 45-caesar-cipher/Solved/caesar-cipher.js
@@ -16,9 +16,13 @@ var caesarCipher = function(str, offset) {
     // NOTE: fromCharCode is a static method on the String object which will convert UTF values into their corresponding characters. The converted character is returned.
     // The first IF statement handles uppercase letters, i.e. UTF values between 65 and 90.
     if (charCode >= 65 && charCode <= 90) {
-      // We are changing the value of offsetChar. We use the static method fromCharCode to grab an affected UTF value. The UTF value is the sum of the var charCode, defined above on line 13, the offset, and -65. I don't know why you subtract 65 or why you % 26.
+      // We are changing the value of offsetChar. We use the static method fromCharCode to grab an affected UTF value. The UTF value is the sum of the var charCode, defined above on line 13, the offset, and -65. That sum is then divided by 26 in order to determine the remainder. The remainder will be the actual offset, since a full rotation around the alphabet is would be 26 characters, hence dividing by 26.
+      // For example, if we had an offset of -1999, this effectively boils down to an actual offset of -23, because to reach an offset of -1999, we would have to loop around the alphabet exactly 76 times (the quotient of -1999 divided by 26), after which there would be a remainder of -23.
+
+      // Removing % 26 from the line below will affect only capital letters. Returns [?] in place.
       offsetChar = String.fromCharCode(((charCode - 65 + offset) % 26) + 65);
     } else if (charCode >= 97 && charCode <= 122) {
+      // Removing % 26 from the line below will affect only lowercase letters.  Returns [?] in place.
       offsetChar = String.fromCharCode(((charCode - 97 + offset) % 26) + 97);
     }
 
