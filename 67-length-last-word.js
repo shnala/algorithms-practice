534b090f97ef362616a51dfb2216fcc726044027
Shawn Analla <shawnanalla@gmail.com>
2022-05-14 15:08:17 -0700

    faster solution for 67

--- 67-length-last-word.js
+++ 67-length-last-word.js
@@ -7,25 +7,19 @@
 // walk backwards from end of 's' until the first instance of space after a letter
 // avoid using split in order to use less memory
  var lengthOfLastWord = function(s) {
-    let split = s.split("");
     let count = 0;
     
     // Remove any unecessary spaces
-    while (split[split.length - 1] === " ") {
-        split.pop();
-        console.log('Removing spaces...');
-    };
-    console.log(split);
-
+    let trim = s.trimEnd();
 
     // This needs to go here instead of at the top in order to pass test where s = "a " so that the space at the end is removed
-    if (!split.includes(" ")) {
-        return split.length;
+    if (!trim.includes(" ")) {
+        return trim.length;
     };
     
     // Walk backwards from 's' until space
-    for (let i = split.length -1; i > 0; i--) {
-        const currentChar = split[i];
+    for (let i = trim.length -1; i > 0; i--) {
+        const currentChar = trim[i];
         if (currentChar === " ") {
             break;
         } else if (currentChar !== " ") {
@@ -36,14 +30,15 @@
 
     return count;
 };
+// done, faster than 53%, smaller than 5%
 
-// const s = "luffy is still joyboy";
+const s = "luffy is still joyboy";
 // should return 6
 
 // const s = "   fly me   to   the moon  ";
 // should return 4
 
-const s = "a ";
+// const s = "a ";
 // should return 1
 
 console.log(lengthOfLastWord(s));
