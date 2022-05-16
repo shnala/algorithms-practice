6fe7bddb829d570a3606ed3f24020bf6c084f384
Shawn Analla <shawnanalla@gmail.com>
2022-05-16 14:52:02 -0700

    imperfect solution

--- 71-merge-sorted-array.js
+++ 71-merge-sorted-array.js
@@ -7,41 +7,41 @@
  */
 
 // kinda similar to exercise 24 except that nums1 must be modified in-place
+// use shift() to move elements from nums2 to nums1
  var merge = function(nums1, m, nums2, n) {
-    let idx1 = 0;
-    let idx2 = 0;
-    let legnth = m + n;
+    let idx = 0;
+
+    if (m === 0) {
+        nums1 = nums2
+        console.log(nums1);
+        return nums1;
+    }
 
     while (nums2.length !== 0) {
-        if (nums1[idx1] === nums2[idx2]) {
-            // when values are the same, push nums2[idx2] into the next slot after nums1[idx1]
-            nums1.splice([idx1], 0, nums2.shift());
-            idx1+=2;
-            continue;
-        }
-
-        if (nums1[idx1] > nums2[idx2]) {
-            nums1.splice([idx1 - 1], 0, nums2.shift());
-            idx1++;
-            continue;
-        }
-
-        if ( nums1[idx1] < nums2[idx2]) {
-            nums1.splice([idx1], 0, nums2.shift());
-            idx1++;
-            continue;
+        if (nums2[0] > nums1[idx] && idx <= m) {
+            idx++;
+        } else {
+            nums1.splice([idx], 0, nums2.shift());
+            idx++;
         }
     }
-    
-    console.log(idx1);
-    return nums1
-};
 
-const nums1 = [1,2,3,0,0,0]
-const m = 3
-const nums2 = [2,5,6]
-const n = 3
+    nums1.splice(m+n, n);
+    return nums1;
+};
+// almost works but fails test where nums1 = [0] and nums2 = [1] but cannot discern why?
+
+// const nums1 = [1,2,3,0,0,0]
+// const m = 3
+// const nums2 = [2,5,6]
+// const n = 3
 // should return nums1 = [1,2,2,3,5,6]
+
+const nums1 = [0];
+const m = 0;
+const nums2 = [1];
+const n = 1;
+// should return nums1 = [1]
 
 console.log(merge(nums1, m, nums2, n));
 
