4c21cd835aba1149f4dc4f90288eed091bc61197
Shawn Analla <shawnanalla@gmail.com>
2022-05-23 19:01:55 -0700

    74 done feelsgooooooood

--- 74-pascals-triangle.js
+++ 74-pascals-triangle.js
@@ -8,17 +8,23 @@
 // while n = 1 || n = 2, x = 1
 // while n <= numRows && n > 2, iterate thru loop while increasing n by 1 upon each iteration
 // when the loop iterates, x will equal x + (x - 1) from the previous row
- var generate = function(numRows) {
+var generate = function (numRows) {
     // initialize triangle
-    let triangle = [[1], [1,1]];
+    let triangle = [[1], [1, 1]];
+    // n will be our marker of sorts, used for iteration. Calculations begin at row 3, hence n = 3
     let n = 3
-    let upperRow = triangle[n-2]
+
+    // edgecases for performance; 1 <= numRows <= 30
+    if (numRows === 1) {
+        return [[1]];
+    }
 
     // need to push array of length 'n' to triangle; array should be [1, _, 1], whereby '_' is of value triangle[n-1][0] + triangle[n-1][1]
-
     while (n < numRows + 1) {
         // initialize a row of length 'n' and set the first and last values to '1'
         let row = new Array(n)
+        // upperRow will initiate at triangle[1], which is [1,1]. Upon iteration of the first loop, n will increment to 4, so upperRow will become triangle[2]. Note that this needs to be manually updated upon each iteration, otherwise upperRow will remain at triangle[1]
+        let upperRow = triangle[n - 2]
 
         for (let i = 0; i < row.length; i++) {
             // const number = row[i];
@@ -39,9 +45,10 @@
     return triangle;
 };
 
-const numRows = 4;
+const numRows = 3;
 
 // const numRows = 5;
 // should output [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]];
 
 console.log(generate(numRows));
+// done, faster than 23%, smaller than 75% ヾ(^▽^*)))
