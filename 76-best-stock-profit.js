d494496502bd344b96968d649e26c5b5f852bf4b
Shawn Analla <shawnanalla@gmail.com>
2022-05-24 18:22:59 -0700

    initalizing 76

---
+++ 76-best-stock-profit.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+*/
+// given array 'prices', return the greatest difference obtainable when iterating through each index in ascending order, i.e. can only move from prices[0] to index prices[prices.length -1]
+// loop through the array
+// compare 
+ var maxProfit = function(prices) {
+    
+};
+
+const prices = [7,1,5,3,6,4];
+// should return 5; profit = 6 - 1; bought on day 1, sold on day 4
+
+console.log(maxProfit(prices));;
