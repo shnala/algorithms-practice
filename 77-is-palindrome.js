e76534d98956b15c356d9c148c82f3ad92c156a5
Shawn Analla <shawnanalla@gmail.com>
2022-05-25 17:35:27 -0700

    initializing

---
+++ 77-is-palindrome.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+
+// similar to previous palindrome exercise, however, this function must remove all spacing and punctuation and evaluate the string thereafter.
+// e.g., s = "A man, a plan, a canal: Panama" should return true, b/c the function will reduce it to "amanaplanacanalpanama" which is the same forwards and backwards.
+ var isPalindrome = function(s) {
+    
+};
+
+const s = "A man, a plan, a canal: Panama";
+// should return true
+
+console.log(isPalindrome(s));
