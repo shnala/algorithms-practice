cf46005fcb2687a577d63a6dd0c188ed22910238
Shawn Analla <shawnanalla@gmail.com>
2022-05-27 16:40:16 -0700

    faster 25%, smaller 25%

--- 78-single-number.js
+++ 78-single-number.js
@@ -5,7 +5,8 @@
 // there is only one number in array 'nums' that isn't a duplicate. Return that number
 // map the array, counting instances of each number in 'nums'
 // find the number in the map that has a value of '1' indicating that it only occurred once
- var singleNumber = function(nums) {
+// solution should be O(n) complexity
+var singleNumber = function (nums) {
     // edgecases
     if (nums.length === 1) return nums[0];
 
@@ -20,15 +21,17 @@
         }
     };
 
-   for (const number in numMap) {
-       if (Object.hasOwnProperty.call(numMap, number)) {
-        //    const number = numMap[number];
-           if (numMap[number] === 1) return Number(number);
-       }
-   }
-};
+    for (const number in numMap) {
+        if (numMap[number] === 1) return Number(number);
 
-const nums = [4,1,2,1,2];
+    }
+};
+// faster than 25%, smaller than 26%, could be better
+
+// const nums = [4, 1, 2, 1, 2];
 // should return 4
+
+// const nums = [2,2,1];
+// should return 1
 
 console.log(singleNumber(nums));
